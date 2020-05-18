33dfe5f344fe877e21eef1cd591b34682a611673
paulgoergen <paulgoergen@paulgoergensair.lan>
2020-05-17 23:24:03 -0700

    Updates

---
+++ precache-manifest.3865a43b702c5e62c1c0f056ae8a5a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b43694ae5cbf4c7655a9968da10bfee",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6735bad5b1c7f6ebb512",
+    "url": "/react-monsters-rolodex/static/css/main.f8b19f1d.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/react-monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6735bad5b1c7f6ebb512",
+    "url": "/react-monsters-rolodex/static/js/main.e1ba6223.chunk.js"
+  },
+  {
+    "revision": "5d1bcaa8ffa01b561da5",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.ab59da59.js"
+  }
+]);
